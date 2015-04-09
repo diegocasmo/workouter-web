d7f9c98fc7783b2cda78706c953e1788afb05540
Diego Castillo <diegocasmo@gmail.com>
2015-04-09 09:14:43 -0500

    added grunt task to run tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -235,6 +235,10 @@ module.exports = function (grunt) {
         }
     });
 
+    grunt.registerTask('test', [
+        'blanket_mocha'
+    ]);
+
     grunt.registerTask('createDefaultTemplate', function () {
         grunt.file.write('.tmp/scripts/templates.js', 'this.JST = this.JST || {};');
     });
