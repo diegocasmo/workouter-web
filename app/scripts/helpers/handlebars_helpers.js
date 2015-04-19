1c7b4beca0d45f4ede469aa419e7bc82c2d66aa3
Diego Castillo <diegocasmo@gmail.com>
2015-04-19 13:51:02 -0500

    Modified beutify date according to design

--- app/scripts/helpers/handlebars_helpers.js
+++ app/scripts/helpers/handlebars_helpers.js
@@ -6,46 +6,22 @@
 define([
   'jquery',
   'underscore',
-  'handlebars'
-], function($, _, Handlebars) {
+  'handlebars',
+  'lang/en_locale'
+], function($, _, Handlebars, enLocale) {
 
   'use strict';
 
   Handlebars.registerHelper('beautify_date', function(options) {
-    var timeAgo = new Date(parseInt(options.fn(this)));
-    if (Object.prototype.toString.call(timeAgo) === '[object Date]') {
-      if (isNaN(timeAgo.getTime())) {
-        return 'Not Valid';
-      } else {
-        var seconds = Math.floor((new Date() - timeAgo) / 1000),
-          intervals = [
-            Math.floor(seconds / 31536000),
-            Math.floor(seconds / 2592000),
-            Math.floor(seconds / 86400),
-            Math.floor(seconds / 3600),
-            Math.floor(seconds / 60)
-          ],
-          times = [
-            'year',
-            'month',
-            'day',
-            'hour',
-            'minute'
-          ];
+    var timeAgo = new Date(parseInt(options.fn(this))),
+        days = enLocale.daysNames[0],
+        months = enLocale.monthsNames[0];
 
-        var key;
-        for (key in intervals) {
-          if (intervals[key] > 1)
-            return intervals[key] + ' ' + times[key] + 's ago';
-          else if (intervals[key] === 1)
-            return intervals[key] + ' ' + times[key] + ' ago';
-        }
+    var dayNumber = timeAgo.getDay() + 1,
+        dayName = days[ timeAgo.getDay() ],
+        monthName = months[ timeAgo.getMonth() ];
 
-        return Math.floor(seconds) + ' seconds ago';
-      }
-    } else {
-      return 'Not Valid';
-    }
+    return dayName + ', '  + dayNumber + ' ' + monthName;
   });
 
 });
--- app/scripts/lang/en_locale.js
+++ app/scripts/lang/en_locale.js
@@ -8,6 +8,29 @@
 /*global define*/
 define({
   appName: 'Workouter',
+  daysNames:[{
+    0: 'Sunday',
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday'
+  }],
+  monthsNames: [{
+    0: 'January',
+    1: 'February',
+    2: 'March',
+    3: 'April',
+    4: 'May',
+    5: 'June',
+    6: 'July',
+    7: 'August',
+    8: 'September',
+    9: 'October',
+    10: 'November',
+    11: 'December'
+  }],
   userModel: {
     uid: {
       required: 'Invalid uid.',
