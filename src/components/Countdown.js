9ac1b445b2e37dbe0226ca7985b56338ca9a4546
Diego Castillo <diegocasmo@gmail.com>
2019-02-06 14:33:58 +0100

    <Countdown/> uses hooks (#43)

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,54 +1,27 @@
-import React, {Component} from 'react'
+import React, {useState, useEffect} from 'react'
 const moment = require('moment')
 
-export class Countdown extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      now: moment().valueOf()
+export function Countdown ({finishAt, onCountdownComplete}) {
+  const [now, setNow] = useState(moment().valueOf())
+  useEffect(() => {
+    if(moment(now).isAfter(moment(finishAt))) {
+      onCountdownComplete()
+    } else {
+      const id = setInterval(() => setNow(moment().valueOf()), 1000)
+      return () => clearInterval(id)
     }
-  }
+  }, [now])
 
-  componentDidMount() {
-    this.configureCountdown()
-  }
-
-  componentWillUnmount() {
-    if(this.countdownId) {
-      clearInterval(this.countdownId)
-    }
-  }
-
-  componentDidUpdate() {
-    if(moment(this.state.now).isAfter(moment(this.props.finishAt))) {
-      if(this.countdownId) {
-        clearInterval(this.countdownId)
-      }
-      this.props.onCountdownComplete()
-    }
-  }
-
-  configureCountdown() {
-    const millisecondsInASecond = 1000
-    this.countdownId = setInterval(() => {
-      this.setState({
-        now: moment().valueOf()
-      })
-    }, millisecondsInASecond)
-  }
-
-  render() {
-    const elapsedMs = moment.duration(this.props.finishAt - this.state.now, 'milliseconds')
-    const hours = moment.duration(elapsedMs).hours()
-    const minutes = moment.duration(elapsedMs).minutes()
-    const seconds = moment.duration(elapsedMs).seconds()
-    const zeroPad = (x) => x > 9 ? x : `0${x}`
-    return (
-      <div className='wkr-countdown'>
-        <span className='wkr-countdown__hours'>{zeroPad(hours)}</span>:
-        <span className='wkr-countdown__minutes'>{zeroPad(minutes)}</span>:
-        <span className='wkr-countdown__seconds'>{zeroPad(seconds)}</span>
-      </div>
-    )
-  }
+  const elapsedMs = moment.duration(finishAt - now, 'milliseconds')
+  const hours = moment.duration(elapsedMs).hours()
+  const minutes = moment.duration(elapsedMs).minutes()
+  const seconds = moment.duration(elapsedMs).seconds()
+  const zeroPad = (x) => x > 9 ? x : `0${x}`
+  return (
+    <div className='wkr-countdown'>
+      <span className='wkr-countdown__hours'>{zeroPad(hours)}</span>:
+      <span className='wkr-countdown__minutes'>{zeroPad(minutes)}</span>:
+      <span className='wkr-countdown__seconds'>{zeroPad(seconds)}</span>
+    </div>
+  )
 }
--- src/components/__tests__/Countdown.spec.js
+++ src/components/__tests__/Countdown.spec.js
@@ -1,14 +1,16 @@
-const moment = require('moment')
 import React from 'react'
+import {act} from 'react-dom/test-utils'
 import sinon from 'sinon'
 import {expect} from 'chai'
-import {shallow} from 'enzyme'
+import {mount} from 'enzyme'
 import {Countdown} from '../Countdown'
+const moment = require('moment')
 
 describe('<Countdown/>', () => {
 
-  let props
   let clock
+  let props
+  let wrapper
   beforeEach(() => {
     const now = moment()
     props = {
@@ -20,28 +22,40 @@ describe('<Countdown/>', () => {
 
   afterEach(() => {
     clock.restore()
+    props = null
+    wrapper = null
   })
 
   it('renders', () => {
-    const wrapper = shallow(<Countdown {...props}/>)
+    act(() => {
+      wrapper = mount(<Countdown {...props}/>)
+    })
     expect(wrapper.find('.wkr-countdown__hours').text()).to.be.equal('00')
     expect(wrapper.find('.wkr-countdown__minutes').text()).to.be.equal('00')
     expect(wrapper.find('.wkr-countdown__seconds').text()).to.be.equal('10')
   })
 
   it('updates countdown', () => {
-    const wrapper = shallow(<Countdown {...props}/>)
+    act(() => {
+      wrapper = mount(<Countdown {...props}/>)
+    })
     expect(wrapper.find('.wkr-countdown__seconds').text()).to.be.equal('10')
     // Assume 1.5 seconds have passed
-    clock.tick(1.5 * 1000)
+    act(() => {
+      clock.tick(1.5 * 1000)
+    })
     expect(wrapper.find('.wkr-countdown__seconds').text()).to.be.equal('09')
   })
 
   it("calls 'onCountdownComplete' when countdown is finished", () => {
-    const wrapper = shallow(<Countdown {...props}/>)
+    act(() => {
+      wrapper = mount(<Countdown {...props}/>)
+    })
     expect(props.onCountdownComplete.called).to.be.false
     // Assume 12 seconds have passed
-    clock.tick(12 * 1000)
+    act(() => {
+      clock.tick(12 * 1000)
+    })
     expect(props.onCountdownComplete.calledOnce).to.be.true
   })
 })
