13b81755df48e4cbd8a6241fce1938c5acd694f0
Diego Castillo <diegocasmo@gmail.com>
2019-06-21 09:39:15 +0200

    Simplified user context (#117)

--- src/context/user-context.js
+++ src/context/user-context.js
@@ -1,78 +1,42 @@
-import React, {createContext, useContext, useReducer} from 'react'
-import {GraphQLClient} from 'graphql-request'
-import {CURRENT_USER_QUERY} from '../graphql/queries'
-import {GraphQLUrl} from '../hooks/graphql-client'
-import {FullPageSpinner} from '../components/UI/FullPageSpinner'
+import React, { createContext, useContext, useReducer } from 'react'
+import { GraphQLClient } from 'graphql-request'
+import { CURRENT_USER_QUERY } from '../graphql/queries'
+import { GraphQLUrl } from '../hooks/graphql-client'
+import { FullPageSpinner } from '../components/UI/FullPageSpinner'
 
 const UserContext = createContext()
 
-export const ACTIONS = {
-  LOGIN_INIT   : 'USER__LOGIN_INIT',
-  LOGIN_SUCCESS: 'USER__LOGIN_SUCCESS',
-  LOGIN_FAILURE: 'USER__LOGIN_FAILURE',
-  LOGOUT       : 'USER__LOGOUT'
-}
-
-function reducer(state, {type, payload}) {
-  switch (type) {
-    case ACTIONS.LOGIN_INIT:
-      return {
-        ...state,
-        isLoading: true
-      }
-    case ACTIONS.LOGIN_SUCCESS:
-      return {
-        ...state,
-        user: payload,
-        isLoading: false
-      }
-    case ACTIONS.LOGIN_FAILURE:
-    case ACTIONS.LOGOUT:
-      return {
-        ...state,
-        user: null,
-        isLoading: false
-      }
-    default:
-      return state
-  }
-}
-
-export const UserProvider = ({
-  // Allow to set initial state via props (useful for testing)
-  user = null, isLoading = false,
-  ...props
-}) => {
-  const [state, dispatch] = useReducer(reducer, {user, isLoading})
+// Allow to set initial state via props (useful for testing)
+export const UserProvider = ({ user = null, isLoading = false, ...props }) => {
+  const [state, setState] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    { user, isLoading }
+  )
 
   const login = async googleUser => {
     try {
-      dispatch({type: ACTIONS.LOGIN_INIT})
+      setState({ isLoading: true })
       const client = new GraphQLClient(GraphQLUrl, {
         headers: {
           authorization: googleUser.getAuthResponse().id_token
         }
       })
 
-      const {currentUser} = await client.request(CURRENT_USER_QUERY)
-      dispatch({type: ACTIONS.LOGIN_SUCCESS, payload: currentUser})
+      const { currentUser } = await client.request(CURRENT_USER_QUERY)
+      setState({ isLoading: false, user: currentUser })
     } catch (err) {
-      dispatch({type: ACTIONS.LOGIN_FAILURE})
+      setState({ isLoading: false, user: null })
       return Promise.reject(new Error(err))
     }
   }
 
-  const logout = () => { dispatch({type: ACTIONS.LOGOUT}) }
+  const logout = () => { setState({ isLoading: false, user: null }) }
 
   return state.isLoading
     ? <FullPageSpinner text='Loading profile...'/>
     : <UserContext.Provider
-        value={{
-          user: state.user,
-          login,
-          logout
-        }}
-        {...props}/>
+        value={{ user: state.user, login, logout }}
+        { ...props }/>
 }
 
 export const useUser = () => {
